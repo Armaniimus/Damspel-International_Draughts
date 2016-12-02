ebeee2f6db13efb6546185cf5e5e327850df19ac
Armand-4A6A <517226@edu.rocmn.nl>
2016-12-02 06:03:02 +0100

    replaced the 2dimensional position array  and made reset function smaller by using the commands switch, case and break.

--- checkers-javascript.js
+++ checkers-javascript.js
@@ -4,6 +4,7 @@ var white = "<center>"+piece.fontcolor("white")+"</center>"
 
 var txt = ""
 var temp = ""
+var temp_two = []
 var clor = black
 var testdata = ""
 var printdisplay = ""
@@ -13,345 +14,380 @@ var square ="_square_"
 
 var rownumb = ""
 var squarenumb = ""
-var h_arraypos = "h_"
 
 var numb_array =["0","one","two","three","four","five","six","seven","eight","nine","ten"]
-var positionarray = [h_zero = [],h_one = [],h_two =[],h_three =[],h_four = [],h_five =[],h_six = [],h_seven = [],h_eight =[],h_nine = [],h_ten = []]
+var positionarray = [ 'row_zero', 'row_one' ,'row_two', 'row_three','row_four','row_five','row_six','row_seven','row_eight','row_nine','row_ten']
+positionarray[0] = []
+positionarray[1] = []
+positionarray[2] = []
+positionarray[3] = []
+positionarray[4] = []
+positionarray[5] = []
+positionarray[6] = []
+positionarray[7] = []
+positionarray[8] = []
+positionarray[9] = []
+positionarray[10] = []
 
 
-var heightpos = 0
-var withpos = 0
-var i = 0
+var y_pos = 0
+var x_pos = 0
+var stoneid = 0		//0=empty	1=white		2=black
+
+///////////////////////////////////////////////////////
+
+function singleplayer(){
+	reset()
+}
+
+function multiplayer(){
+	reset_id()
+}
 
 function reset(){
-	clor = white
 	printdisplay = ""
 	returntodisplay()
-	for(heightpos =1; heightpos < 11; heightpos++){
+	for(y_pos =1; y_pos < 11; y_pos ++){
 		
-		if(heightpos > 5){
-			clor=black
+		switch(y_pos){			
+			case 1:
+				clor = white
+				break
+			case 5:
+				clor = ""
+				break
+			case 7:	
+				clor = black
+				break
 		}
+			
+		for(x_pos = 2; x_pos < 11; x_pos++){
+			temp = "row_"+numb_array[y_pos]+"_square_"+numb_array[x_pos]
+			document.getElementById(temp).innerHTML = clor
+			x_pos ++
+		}
+		y_pos ++
+
+		for(x_pos = 1; x_pos < 11; x_pos++){
+			temp = "row_"+numb_array[y_pos]+"_square_"+numb_array[x_pos]
+			document.getElementById(temp).innerHTML = clor
+			x_pos ++
+		}	
+	}
+}
+function reset_id(){
+	
+	for(y_pos =1; y_pos < 11; y_pos ++){
 		
-		if(heightpos < 5 || heightpos > 6 ){
+		switch(y_pos){			
+			case 1:
+				stoneid = 1	
+				break
+			case 5:
+				stoneid = 0
+				break
+			case 7:	
+				stoneid = 2
+				break
+		}	
+
+		for(x_pos = 2; x_pos < 11; x_pos ++){
+			temp = "row_"+numb_array[y_pos]+"_square_"+numb_array[x_pos]
+
+			positionarray[ y_pos ][ x_pos ] = stoneid
 			
-			for(withpos = 2; withpos < 11; withpos++){
-				temp = "row_"+numb_array[heightpos]+"_square_"+numb_array[withpos]
-				document.getElementById(temp).innerHTML = clor
-				withpos ++
-			}
-			heightpos ++
-			
-			for(withpos = 1; withpos < 11; withpos++){
-				temp = "row_"+numb_array[heightpos]+"_square_"+numb_array[withpos]
-				document.getElementById(temp).innerHTML = clor
-				withpos ++
-			}
+			document.getElementById(temp).innerHTML = positionarray[ y_pos ][ x_pos ] = stoneid
+
+			x_pos ++
 		}
-		else{ 
-			for(withpos = 2; withpos < 11; withpos++){
-				temp = "row_"+numb_array[heightpos]+"_square_"+numb_array[withpos]
-				document.getElementById(temp).innerHTML = ""
-				withpos ++
-			}
-			heightpos ++
+		y_pos ++
+
+		for(x_pos = 1; x_pos < 11; x_pos++){
+			temp = "row_"+numb_array[y_pos]+"_square_"+numb_array[x_pos]
 			
-			for(withpos = 1; withpos < 11; withpos++){
-				temp = "row_"+numb_array[heightpos]+"_square_"+numb_array[withpos]
-				document.getElementById(temp).innerHTML = ""
-				withpos ++
-			}
+			positionarray[ y_pos ][ x_pos ] = stoneid
+
+			document.getElementById(temp).innerHTML = positionarray[ y_pos ][ x_pos ] = stoneid 
 			
-			
+			x_pos ++
 		}
 		
 	}
 }
-function sleep(){
-	setTimeout(sleeptwo(), 1000)
-}	
-function sleeptwo(){
-	temp=temp
-}
-function write_piece(){
-		rownumb = "row_"+numb_array[heightpos]
-		squarenumb = "_square_"+numb_array[withpos]
-		document.getElementById(rownumb+squarenumb).innerHTML = clor
-		returntodisplay()
-}
-function singleplayer(){
-	reset()
-}
-function multiplayer(){
-	reset()
-}
-function returntodisplay(){
-	document.getElementById("display").innerHTML = printdisplay
-}
-function execute(){
-	temp = h_arraypos + numb_array [ heightpos ] //defines heightposition
-	positionarray[ temp [ withpos ] ] = "Height = " + heightpos + " Width = " + withpos //writes data to array
 	
-	testdata = positionarray [ temp [ withpos ] ]
-	printdisplay = testdata
+
+function execute(){
+	//temp = "Height = " + y_pos + " Width = " + x_pos
+	
+	printdisplay = positionarray[ y_pos ][ x_pos ]
 	returntodisplay()
 	write_piece()
 }
 
+function returntodisplay(){
+	document.getElementById("display").innerHTML = printdisplay
+}
+
+function write_piece(){
+	rownumb = "row_"+numb_array[y_pos]
+	squarenumb = "_square_"+numb_array[x_pos]
+	document.getElementById(rownumb+squarenumb).innerHTML = clor
+}
+//////////////////////////////////////////////////////////////////
 function row_ten___square_one(){
-	heightpos = 10
-	withpos = 1
+	y_pos = 10
+	x_pos = 1
 	execute()
 }
 function row_ten___square_three(){
-	heightpos = 10
-	withpos = 3
+	y_pos = 10
+	x_pos = 3
 	execute()
 }
 function row_ten___square_five(){
-	heightpos = 10
-	withpos = 5
+	y_pos = 10
+	x_pos = 5
 	execute()
 }
 function row_ten___square_seven(){
-	heightpos = 10
-	withpos = 7
+	y_pos = 10
+	x_pos = 7
 	execute()
 }
 function row_ten___square_nine(){
-	heightpos = 10
-	withpos = 9
+	y_pos = 10
+	x_pos = 9
 	execute()
 }
 function row_nine___square_two(){
-	heightpos = 9
-	withpos = 2
+	y_pos = 9
+	x_pos = 2
 	execute()
 }
 function row_nine___square_four(){
-	heightpos = 9
-	withpos = 4
+	y_pos = 9
+	x_pos = 4
 	execute()
 }
 function row_nine___square_six(){
-	heightpos = 9
-	withpos = 6
+	y_pos = 9
+	x_pos = 6
 	execute()
 }
 function row_nine___square_eight(){
-	heightpos = 9
-	withpos = 8
+	y_pos = 9
+	x_pos = 8
 	execute()
 }
 function row_nine___square_ten(){
-	heightpos = 9
-	withpos = 10
+	y_pos = 9
+	x_pos = 10
 	execute()
 }
 function row_eight___square_one(){
-	heightpos = 8
-	withpos = 1
+	y_pos = 8
+	x_pos = 1
 	execute()
 }
 function row_eight___square_three(){
-	heightpos = 8
-	withpos = 3
+	y_pos = 8
+	x_pos = 3
 	execute()
 }
 function row_eight___square_five(){
-	heightpos = 8
-	withpos = 5
+	y_pos = 8
+	x_pos = 5
 	execute()
 }
 function row_eight___square_seven(){
-	heightpos = 8
-	withpos = 7
+	y_pos = 8
+	x_pos = 7
 	execute()
 }
 function row_eight___square_nine(){
-	heightpos = 8
-	withpos = 9
+	y_pos = 8
+	x_pos = 9
 	execute()
 }
 //row 7
 function row_seven___square_two(){
-	heightpos = 7
-	withpos = 2
+	y_pos = 7
+	x_pos = 2
 	execute()
 }
 function row_seven___square_four(){
-	heightpos = 7
-	withpos = 4
+	y_pos = 7
+	x_pos = 4
 	execute()
 }
 function row_seven___square_six(){
-	heightpos = 7
-	withpos = 6
+	y_pos = 7
+	x_pos = 6
 	execute()
 }
 function row_seven___square_eight(){
-	heightpos = 7
-	withpos = 8
+	y_pos = 7
+	x_pos = 8
 	execute()
 }
 function row_seven___square_ten(){
-	heightpos = 7
-	withpos = 10
+	y_pos = 7
+	x_pos = 10
 	execute()
 }
 //row 6
 function row_six___square_one(){
-	heightpos = 6
-	withpos = 1
+	y_pos = 6
+	x_pos = 1
 	execute()
 }
 function row_six___square_three(){
-	heightpos = 6
-	withpos = 3
+	y_pos = 6
+	x_pos = 3
 	execute()
 }
 function row_six___square_five(){
-	heightpos = 6
-	withpos = 5
+	y_pos = 6
+	x_pos = 5
 	execute()
 }
 function row_six___square_seven(){
-	heightpos = 6
-	withpos = 7
+	y_pos = 6
+	x_pos = 7
 	execute()
 }
 function row_six___square_nine(){
-	heightpos = 6
-	withpos = 9
+	y_pos = 6
+	x_pos = 9
 	execute()
 }
 //row 5
 function row_five___square_two(){
-	heightpos = 5
-	withpos = 2
+	y_pos = 5
+	x_pos = 2
 	execute()
 }
 function row_five___square_four(){
-	heightpos = 5
-	withpos = 4
+	y_pos = 5
+	x_pos = 4
 	execute()
 }
 function row_five___square_six(){
-	heightpos = 5
-	withpos = 6
+	y_pos = 5
+	x_pos = 6
 	execute()
 }
 function row_five___square_eight(){
-	heightpos = 5
-	withpos = 8
+	y_pos = 5
+	x_pos = 8
 	execute()
 }
 function row_five___square_ten(){
-	heightpos = 5
-	withpos = 10
+	y_pos = 5
+	x_pos = 10
 	execute()
 }
 //row 4
 function row_four___square_one(){
-	heightpos = 4
-	withpos = 1
+	y_pos = 4
+	x_pos = 1
 	execute()
 }
 function row_four___square_three(){
-	heightpos = 4
-	withpos = 3
+	y_pos = 4
+	x_pos = 3
 	execute()
 }
 function row_four___square_five(){
-	heightpos = 4
-	withpos = 5
+	y_pos = 4
+	x_pos = 5
 	execute()
 }
 function row_four___square_seven(){
-	heightpos = 4
-	withpos = 7
+	y_pos = 4
+	x_pos = 7
 	execute()
 }
 function row_four___square_nine(){
-	heightpos = 4
-	withpos = 9
+	y_pos = 4
+	x_pos = 9
 	execute()
 }
 //row 3
 function row_three___square_two(){
-	heightpos = 3
-	withpos = 2
+	y_pos = 3
+	x_pos = 2
 	execute()
 }
 function row_three___square_four(){
-	heightpos = 3
-	withpos = 4
+	y_pos = 3
+	x_pos = 4
 	execute()
 }
 function row_three___square_six(){
-	heightpos = 3
-	withpos = 6
+	y_pos = 3
+	x_pos = 6
 	execute()
 }
 function row_three___square_eight(){
-	heightpos = 3
-	withpos = 8
+	y_pos = 3
+	x_pos = 8
 	execute()
 }
 function row_three___square_ten(){
-	heightpos = 3
-	withpos = 10
+	y_pos = 3
+	x_pos = 10
 	execute()
 }
 //row 2
 function row_two___square_one(){
-	heightpos = 2
-	withpos = 1
+	y_pos = 2
+	x_pos = 1
 	execute()
 }
 function row_two___square_three(){
-	heightpos = 2
-	withpos = 3
+	y_pos = 2
+	x_pos = 3
 	execute()
 }
 function row_two___square_five(){
-	heightpos = 2
-	withpos = 5
+	y_pos = 2
+	x_pos = 5
 	execute()
 }
 function row_two___square_seven(){
-	heightpos = 2
-	withpos = 7
+	y_pos = 2
+	x_pos = 7
 	execute()
 }
 function row_two___square_nine(){
-	heightpos = 2
-	withpos = 9
+	y_pos = 2
+	x_pos = 9
 	execute()
 }
 function row_one___square_two(){
-	heightpos = 1
-	withpos = 2
+	y_pos = 1
+	x_pos = 2
 	execute()
 }
 //row 1
 function row_one___square_four(){
-	heightpos = 1
-	withpos = 4
+	y_pos = 1
+	x_pos = 4
 	execute()
 }
 function row_one___square_six(){
-	heightpos = 1
-	withpos = 6
+	y_pos = 1
+	x_pos = 6
 	execute()
 }
 function row_one___square_eight(){
-	heightpos = 1
-	withpos = 8
+	y_pos = 1
+	x_pos = 8
 	execute()
 }
 function row_one___square_ten(){
-	heightpos = 1
-	withpos = 10
+	y_pos = 1
+	x_pos = 10
 	execute()
 }
